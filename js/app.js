96486bd45c141f4fef8a7ba11549a85969d88f58
Kevin Mahoney <kjmahoney13@gmail.com>
2016-12-09 10:08:58 -0500

    official portfolio initial commit

---
+++ js/app.js
@@ -0,0 +1,18 @@
+$(document).foundation()
+
+
+function checkScroll(){
+    var startY = $('.navbar').height() * 1; //The point where the navbar changes in px
+
+    if($(window).scrollTop() < startY){
+        $('.navbar').addClass("scrolled");
+    }else{
+        $('.navbar').removeClass("scrolled");
+    }
+}
+
+if($('.navbar').length > 0){
+    $(window).on("scroll load resize", function(){
+        checkScroll();
+    });
+}
