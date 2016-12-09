3a34ea4e53e9357844024086f46625b464da38cc
Kevin Mahoney <kjmahoney13@gmail.com>
2016-12-09 16:31:37 -0500

    scrolling navbar fonts corrected

--- js/app.js
+++ js/app.js
@@ -1,13 +1,16 @@
 $(document).foundation()
 
-
 function checkScroll(){
-    var startY = $('.navbar').height() * 1; //The point where the navbar changes in px
+  console.log($(window).scrollTop())
+    var startY = $('.navbar').height() * 2; //The point where the navbar changes in px
 
     if($(window).scrollTop() < startY){
+      // console.log($(window).scrollTop())
         $('.navbar').addClass("scrolled");
+        $('.nav-item').addClass("nav-item-scroll");
     }else{
         $('.navbar').removeClass("scrolled");
+        $('.nav-item').removeClass("nav-item-scroll");
     }
 }
 
